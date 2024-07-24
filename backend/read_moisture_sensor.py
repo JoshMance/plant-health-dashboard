3435b1f842f7b0e1c6f8e0a88b4b54fa899de7fa
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2024-07-24 15:24:14 +0800

    Splitting sensor and web components

--- backend/read_moisture_sensor.py
+++
@@ -1,9 +0,0 @@
-import serial
-
-arduino = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
-
-while True:
-    data = arduino.readline().decode("utf-8")
-    if (data) != '':
-        print(data)
-        break
