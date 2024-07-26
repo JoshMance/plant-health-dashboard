ed667a7631be8f6b3086dc7d732d1118f1acee73
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2024-07-26 12:37:43 +0800

    Setting up a hello world sensor server

--- sensor-server/Interface_test.py
+++ sensor-server/Interface_test.py
@@ -5,6 +5,7 @@ x = Interface(ports=['/dev/ttyACM0', '/dev/ttyACM1'],
               baudrate=9600, 
               timeout=0.1, 
               wait_time = 2)
+
 val = x.get_moisture_values()
 print(val)
 
